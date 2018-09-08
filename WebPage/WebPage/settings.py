1fd366753d7bab5db6e8e8b87cbe2bf5c6aa099b
root <root@Ubuntu1.16.04>
2018-09-08 04:48:58 +0200

    New working server! Zinnia removed!

--- WebPage/WebPage/settings.py
+++ WebPage/WebPage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('secret_KEY')
 
 #Prod
 DEBUG = False
-ALLOWED_HOSTS = ['203.178.143.47', '.robinlunde.tk','.robinlunde.cf']
+ALLOWED_HOSTS = ['95.216.138.178', '.robinlunde.com', '.robinlunde.tk','.robinlunde.cf', 'localhost']
 
 
 # Application definition
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     #zinnia
-    'django_comments',
-    'mptt',
-    'tagging',
-    'zinnia_bootstrap',
-    'zinnia',
+    #'django_comments',
+    #'mptt',
+    #'tagging',
+    #'zinnia_bootstrap4',
+    #'zinnia',
     #own
     'prog.apps.ProgConfig',
     'images.apps.ImagesConfig'
@@ -77,8 +77,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 #zinnia
-                'django.template.context_processors.i18n',
-                'zinnia.context_processors.version',  # Optional
+                #'django.template.context_processors.i18n',
+                #'zinnia.context_processors.version',  # Optional
             ],
             'loaders': [
                 'app_namespace.Loader',
@@ -136,18 +136,20 @@ USE_L10N = True
 USE_TZ = True
 
 # Has to be set to use zinnia
-#SITE_ID = 1
-SITE_ID=2
+SITE_ID = 1
+#SITE_ID=2
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join('/var/www/', 'static/')
+
 
 #
 #E-mail settings
 ADMINS = (
-    ('Robin Lunde', 'robinl@sfc.wide.ad.jp'),
+    ('Robin Lunde', 'robin@wub.no'),
 )
 MANAGERS = ADMINS
